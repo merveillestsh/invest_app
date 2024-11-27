ac01d94d6085bc96ea4b40d12e0070a46888fe04
merveillestsh <ntumbamerveilles2001@gmail.com>
2024-11-27 13:13:33 -0600

    cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,79 +5,44 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Configuración del AnimationController para animación
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2), // Duración de la animación
-    );
-
-    // Animación de escala (zoom)
-    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
-
-    // Iniciar la animación
-    _controller.forward();
-
-    // Navegar a la pantalla principal después de 3 segundos
-    Timer(const Duration(seconds: 10), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => MainScreen()),
-      );
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose(); // Liberar recursos
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white, // Fondo del splash
-      body: Center(
-        child: ScaleTransition(
-          scale: _animation, // Aplicar la animación de zoom
-          child: Image.asset('assets/images/piggy bank.gif', width: 250, height: 250), // Ruta del logo
-        ),
-      ),
-    );
-  }
-}
-
-class MainScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Pantalla Principal')),
-      body: Center(
-        child: Text(
-          'Bienvenido',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      title: 'Material App',
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'InvestApp',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Have fun learning how to save and invest your money',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            Image.asset('assets/images/piggy bank.gif'),
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {}, 
+                  child: Text('Login'),
+                ),
+                ElevatedButton(
+                  onPressed: () {}, 
+                  child: Text('Register'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
